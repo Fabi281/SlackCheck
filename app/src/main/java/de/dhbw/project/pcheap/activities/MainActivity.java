ff91f2a814129f79d3e4c2f1a0a0ad5bfdc6297a
Fabi281 <Fabian.Meindl@freenet.de>
2021-11-14 09:58:49 +0100

    Dummy-Menu (Sorting by price and alphabet(name))

--- app/src/main/java/de/dhbw/project/pcheap/activities/MainActivity.java
+++ app/src/main/java/de/dhbw/project/pcheap/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package de.dhbw.project.pcheap.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import de.dhbw.project.pcheap.pojo.Item;
@@ -35,8 +41,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupRecyclerView();
-
         loadData();
+
+        sortByPrice();
 
     }
 
@@ -67,4 +74,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.SortAlph:
+                sortByName();
+                break;
+            case R.id.SortPrice:
+                sortByPrice();
+                break;
+        }
+        return true;
+    }
+
+    private void sortByName()
+    {
+        Comparator<Item> itemComparatorByName = new Comparator<Item>() {
+            @Override
+            public int compare(Item item1, Item item2) {
+                return item1.getName().compareTo(item2.getName());
+            }
+        };
+        Collections.sort(items, itemComparatorByName);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void sortByPrice()
+    {
+        Comparator<Item> itemComparatorByPrice = new Comparator<Item>() {
+            @Override
+            public int compare(Item item1, Item item2) {
+                return Double.compare(item1.getPrice(), item2.getPrice());
+            }
+        };
+        Collections.sort(items, itemComparatorByPrice);
+        adapter.notifyDataSetChanged();
+    }
+
+
 }
